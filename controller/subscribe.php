d0df8f3282cf42f7dd03eef36cd6fcc0110739a3
Armaklan <lionel.zuber@gmail.com>
2013-06-17 10:51:01 +0200

    #14 - MP lors de l'inscription au site Possibilité d'avoir un MP du système

--- controller/subscribe.php
+++ controller/subscribe.php
@@ -14,7 +14,19 @@ $subscribeController->get('/', function() use($app) {
 
 $subscribeController->post('/save', function(Request $request) use($app) {
     try {
-        $user = $app["userService"]->subscribeUser($request);
+        $app["userService"]->subscribeUser($request);
+        $listUser[0] = $request->get('username');
+        $content = "
+            <p>Bonjour et bienvenue sur la plateforme jdRoll !</p>
+            <br>
+            <p>Dans un premier temps, je vous proposer d'aller vous présenter sur le Forum dans le fils 'Présentation'. Vous pourrez ensuite copier-coller cette présentation dans votre profil (histoire que tout le monde puisse la retrouver facilement).</p>
+            <p>N'hésitez pas à faire un petit coucou sur le tchat (en bas de la page d'accueil ou en bas du Forum).</p>
+            <br>
+            <p>Nous espérons que vous trouverez de quoi vous amuser sur ce forum.</p>
+            <br>
+            <p>Ludiquement</p>
+        ";
+        $app['messagerieService']->sendMessageWith(0, "Système", "Bienvenu sur jdRoll ! ", $content, $listUser);
         return $app->render('login.html.twig', ['error' => "Création de l'utilisateur réussit.", 'url' => '!']);
     } catch (Exception $e) {
         $user = array("username" => $request->get('username'), "mail" => $request->get('mail'));
