a546da24f1d806156921a9b87c72db614f19e8f0
gailin24px <gajaf@hotmail.com>
2013-08-16 00:05:22 -0700

    - Mantis #49 support des caractères accentué et du '%,!' dans les urls

--- service/chatService.php
+++ service/chatService.php
@@ -57,7 +57,7 @@ class ChatService {
     }
 
     private function urllink($content='') {
-        $content = preg_replace('#(((https?://)|(w{3}\.))+[a-zA-Z0-9&;\#\.\?=_/-]+\.([a-z]{2,4})([a-zA-Z0-9&;\#\.\?=_/-]+))#i', '<a href="$0" target="_blank">$0</a>', $content);
+        $content = preg_replace('#(((https?://)|(w{3}\.))+[a-zA-Z0-9&;\#\.\?=_/-]+\.([a-z]{2,4})([[:alnum:]&;%,!\#\.\?=_/-]+))#i', '<a href="$0" target="_blank">$0</a>', $content);
         // Si on capte un lien tel que www.test.com, il faut rajouter le http://
         if(preg_match('#<a href="www\.(.+)" target="_blank">(.+)<\/a>#i', $content)) {
             $content = preg_replace('#<a href="www\.(.+)" target="_blank">(.+)<\/a>#i', '<a href="http://www.$1" target="_blank">www.$1</a>', $content);
