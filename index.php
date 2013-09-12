859d5a1a21b328a82e6dcdd89808e365d3ed52ad
Armaklan <lionel.zuber@gmail.com>
2013-09-12 10:13:25 +0200

    Suppression de Statut Campagn des imports

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
 require __DIR__.'/vendor/autoload.php';
-require __DIR__.'/constante/StatutCampagn.php';
 require __DIR__.'/service/dbService.php';
 require __DIR__.'/service/userService.php';
 require __DIR__.'/service/campagneService.php';
