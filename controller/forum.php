dfdd88e9b61d1959ee49b77eb872437a333364a9
Armaklan <lionel.zuber@gmail.com>
2013-10-02 08:59:53 +0200

    Refactoring autour de la zone private

--- controller/forum.php
+++ controller/forum.php
@@ -167,6 +167,79 @@ $forumController->get('/{campagne_id}/{topic_id}', function($campagne_id, $topic
 	return $app->redirect($app->path('topic_page', array('campagne_id' => $campagne_id, 'topic_id' => $topic_id, 'no_page' => $page)));
 })->bind("topic");
 
+
+function getPrivateZone($txt) {
+    return '<div style="background-color: #EBEADD; background-color: rgba(230, 230, 230, 0.4); padding:15px ">'. $txt . '</div>';
+}
+
+function transformPrivateZoneForMessage(&$post, $login, $perso, $is_mj) {
+        $isThereAPrivateForMe = false;
+        $postForTest = preg_replace_callback('#\[(private|prv)(?:=(.*,?))?\](.*)\[/\1\]#isU',
+        function ($matches) use($is_mj,$login,$perso,&$isThereAPrivateForMe,$post){
+
+                if($is_mj || !isset($perso['name']) || strcasecmp($perso['name'],$post['perso_name']) == 0)
+                {
+                        $isThereAPrivateForMe = true;
+                }
+                else
+                {
+                        $users = preg_split("#,#", $matches[2]);
+                        foreach($users as $user)
+                        {
+                                if(strcasecmp($login,trim($user)) == 0 || strcasecmp($perso['name'],trim($user)) == 0)
+                                {
+                                        $isThereAPrivateForMe=true;
+                                        break;
+                                }
+                        }
+                }
+                return '';
+                },
+                $post['post_content']
+        );
+
+        $postTrim = strip_tags($postForTest);
+        //Trop gourmand ? A optimiser ?
+        $postTrim = strtr($postTrim, array_flip(get_html_translation_table(HTML_ENTITIES)));
+        $postTrim = trim($postTrim,"\t\n\r\0\x0B\xC2\xA0\xE2\x80\x89\xE2\x80\x83\xE2\x80\x82");
+
+        $postSize = strlen($postTrim);
+
+        $post['post_content'] = preg_replace_callback('#\[(private|prv)(?:=(.*,?))?\](.*)\[/\1\]#isU',
+        function ($matches) use ($is_mj,$login,$perso,$post,$postSize,$isThereAPrivateForMe){
+
+                $txt = '<b><p size="small">Visible par : MJ, ' . $matches[2] . '</p></b>';
+                $ret = '';
+                if($is_mj || !isset($perso['name']) || strcasecmp($perso['name'],$post['perso_name']) == 0)
+                {
+                    $ret = getPrivateZone($txt . $matches[3]);
+                }
+                else
+                {
+                        $users = preg_split("#,#", $matches[2]);
+                        foreach($users as $user)
+                        {
+                                if(strcasecmp($login,trim($user)) == 0 || strcasecmp($persoName,trim($user)) == 0)
+                                {
+                                        $ret = getPrivateZone($txt . $matches[3]);
+                                        break;
+                                }
+                        }
+                }
+
+                if(!$isThereAPrivateForMe)
+                {
+                        if($postSize == 0)
+                                $ret = getPrivateZone('<br>Une partie de ce message est en privée et ne vous est pas accessible.<br>');
+                }
+
+                return $ret;
+                },
+                $post['post_content']
+        );
+    return replace_hide($post['post_content']);
+}
+
 $forumController->get('/{campagne_id}/{topic_id}/page/{no_page}', function($campagne_id, $topic_id, $no_page) use($app) {
 	$campagne_id = getInterneCampagneNumber($campagne_id);
 	$posts = $app["postService"]->getPostsInTopic($topic_id, $no_page);
@@ -179,82 +252,13 @@ $forumController->get('/{campagne_id}/{topic_id}/page/{no_page}', function($camp
 	$perso = $app['persoService']->getPersonnage(false, $campagne_id, $app['session']->get('user')['id']);
 	$is_mj = $app["campagneService"]->isMj($campagne_id);
 	$personnages = $app['persoService']->getAllPersonnagesInCampagne($campagne_id);
-	$campagne_id = getExterneCampagneNumber($campagne_id);
 	$last_page = $app["postService"]->getLastPageOfPost($topic_id);
-
-		foreach ($posts as &$post)
-		{
-			if($campagne_id > 0)
-			{
-				$isThereAPrivateForMe = false;
-				$postForTest = preg_replace_callback('#\[(private|prv)(?:=(.*,?))?\](.*)\[/\1\]#isU',
-				function ($matches) use($is_mj,$app,$perso,&$isThereAPrivateForMe,$post){
-
-					if($is_mj || !isset($perso['name']) || strcasecmp($perso['name'],$post['perso_name']) == 0)
-					{
-						$isThereAPrivateForMe = true;
-					}
-					else
-					{
-						$users = preg_split("#,#", $matches[2]);
-						foreach($users as $user)
-						{
-							if(strcasecmp($app['session']->get('user')['login'],trim($user)) == 0 || strcasecmp($perso['name'],trim($user)) == 0)
-							{
-								$isThereAPrivateForMe=true;
-								break;
-							}
-						}
-					}
-					return '';
-					},
-					$post['post_content']
-				);
-				
-				$postTrim = strip_tags($postForTest);
-				//Trop gourmand ? A optimiser ?
-				$postTrim = strtr($postTrim, array_flip(get_html_translation_table(HTML_ENTITIES)));
-				$postTrim = trim($postTrim,"\t\n\r\0\x0B\xC2\xA0\xE2\x80\x89\xE2\x80\x83\xE2\x80\x82");
-				
-				$postSize = strlen($postTrim);
-				
-				$post['post_content'] = preg_replace_callback('#\[(private|prv)(?:=(.*,?))?\](.*)\[/\1\]#isU',
-				function ($matches) use ($is_mj,$app,$perso,$post,$postSize,$isThereAPrivateForMe){
-					
-						$txt = '<b><p size="small">Visible par : MJ, ' . $matches[2] . '</p></b>';
-						 $ret = '';
-					if($is_mj || !isset($perso['name']) || strcasecmp($perso['name'],$post['perso_name']) == 0)
-					{
-						$ret = '<div style="background-color: #EBEADD; background-color: rgba(230, 230, 230, 0.4); padding:15px ">'. $txt . $matches[3] . '</div>';
-					}
-					else
-					{
-						$users = preg_split("#,#", $matches[2]);
-						foreach($users as $user)
-						{
-							if(strcasecmp($app['session']->get('user')['login'],trim($user)) == 0 || strcasecmp($perso['name'],trim($user)) == 0)
-							{
-								$ret = '<div style="background-color: #EBEADD; background-color: rgba(230, 230, 230, 0.4); padding:15px ">'. $txt . $matches[3] . '</div>';
-								
-								break;
-							}
-						}
-					}
-					
-					if(!$isThereAPrivateForMe)
-					{
-						if($postSize == 0)
-							$ret = '<div style="background-color: #EBEADD; background-color: rgba(230, 230, 230, 0.4); padding:15px "><br>Une partie de ce message est en privée et ne vous est pas accessible.<br></div>';
-					}
-					
-					return $ret;
-					},
-					$post['post_content']
-				);
-			}
-			$post['post_content'] = replace_hide($post['post_content']);
-		}
-
+        if($campagne_id > 0) {
+            foreach ($posts as &$post){
+                    transformPrivateZoneForMessage($post, $app['session']->get('user')['login'], $perso, $is_mj);
+            }
+        }
+        $campagne_id = getExterneCampagneNumber($campagne_id);
 	return $app->render('forum_topic.html.twig', ['campagne_id' => $campagne_id, 'topic' => $topic, 'posts' => $posts,
 			'perso' => $perso, 'is_mj' => $is_mj, 'personnages' => $personnages,
 			"last_page" => $last_page, 'actual_page' => $no_page]);
