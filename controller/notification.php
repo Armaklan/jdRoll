a6ba793a9f6377fb5b39f30816f194ba85c03e91
Armaklan <lionel.zuber@gmail.com>
2013-10-09 21:14:10 +0200

    Rechargement ajax des notifications

--- controller/notification.php
+++ controller/notification.php
@@ -9,6 +9,7 @@
 
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /*
  Controller de campagne (sécurisé)
@@ -23,6 +24,17 @@ $notificationController->get('/', function() use($app) {
         $hasNotif = ($nbNotif > 0);
 	return $app->render('notification/list.html.twig', ['notifs' => $notifs, 'has_notif' => $hasNotif, 'nb_notif' => $nbNotif]);
 })->bind("notifications");
+
+$notificationController->get('/ajax', function() use($app) {
+     $user_id = $app["session"]->get('user')['id'];
+	 $notifs = $app['notificationService']->getNotifForUser($app["session"]->get('user')['id']);
+     $nbNotif = count($notifs);
+     $hasNotif = ($nbNotif > 0);
+     $response = new Response();
+     $response->headers->set('Content-Type', 'application/test');
+	 $ret = $app->render('notification/list.html.twig', ['notifs' => $notifs, 'has_notif' => $hasNotif, 'nb_notif' => $nbNotif], $response);
+	 return $ret;
+})->bind("notifications_ajax");
 
 $notificationController->post('/del', function(Request $request) use($app) {
         $id = $request->get('id');
