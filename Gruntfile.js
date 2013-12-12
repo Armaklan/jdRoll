4e5c68cb98258fc5139b54d65cdaa100a50d7ad9
Armaklan <lionel.zuber@armaklan.org>
2013-12-12 22:19:19 +0100

    Mise en place d'un grant avec : - Mode dev avec serve des fichiers - Minification des js
    Correction de coquilles

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+	grunt.loadNpmTasks('grunt-php');
+	grunt.loadNpmTasks('grunt-bower-task');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	grunt.initConfig({
+	    php: {
+		        test: {
+		            options: {
+		                keepalive: true,
+		                open: true
+		            }
+		        }
+		    },
+		bower: {
+		    install: {
+		        options: {
+			        copy: false,
+			    }
+		    }
+		  },
+	    uglify: {
+	    	my_target: {
+	      		files: {
+	        		'js/controller.min.js': [
+	        			'js/tinymce_conf.js',
+	        			'js/controller/onload.js', 
+	        			'js/controller/campagne.js', 
+	        			'js/controller/chat.js', 
+	        			'js/controller/draft.js', 
+	        			'js/controller/notification.js'
+	        		]
+	      		}
+	    	}
+	  	}
+
+	});
+
+	grunt.registerTask('prepare', ['bower','composer']);
+	grunt.registerTask('dev', ['php']);
+	grunt.registerTask('dist', ['uglify']);
+	grunt.registerTask('default', ['php']);
+
+};
+
