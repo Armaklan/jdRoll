f49eab9a4618993ad0e98b79ee0ae9ec56f40ebb
Armaklan <lionel.zuber@armaklan.org>
2014-03-03 16:11:01 +0100

    Transfert Sidebar Layout Resize in UIController

--- js/controller/ui.js
+++ js/controller/ui.js
@@ -11,9 +11,27 @@ var uiControllerImpl = function() {
        $('.select2').select2({width: 'element'});
     }
 
+    function resizeSidebar() {
+        if($(window).width() > 992) {
+            $('.maxheight').css({'height':($(window).height() - 50)+'px'});
+            $('.maxheight').css({'overflow':'auto'});
+        } else {
+            $('.maxheight').css({'height':'100%'});
+            $('.maxheight').css({'overflow':'visible'});
+        }
+    }
+
+    function activateSidebarLayout() {
+        resizeSidebar();
+        $(window).resize(function(){
+            resizeSidebar();
+        });
+    }
+
     return {
         activateUi : function() {
             activateSelect2();
+            activateSidebarLayout();
         }
     };
 };
