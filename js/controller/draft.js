cc2c9385c573ca5de46fe7eed0aee63e12501ea2
Armaklan <lionel.zuber@armaklan.org>
2014-03-03 22:47:15 +0100

    ENHANCEMENT - Add auto save

--- js/controller/draft.js
+++ js/controller/draft.js
@@ -8,26 +8,55 @@
 
 var draftControllerImpl = function() {
 
-    return {
-        ajaxEnreg : function(campagne_id) {
-            topic_id = $("input[name=topic_id]").val();
-            perso_id = $("input[name=perso_id]").val();
-            tinyMCE.triggerSave();
-            content = $("#content").val();
-            
-            $.ajax({
-                type: "POST",
-                url: BASE_PATH + "/forum/" + campagne_id + "/topic/draft",
-                data: {
-                    topic_id: topic_id,
-                    perso_id: perso_id,
-                    content: content
-                },
-                success: function(msg){$("#enregResult").text(msg)},
-                error: function(msg) {}
-            });
+    function getNowHour() {
+        var nowDate = new Date();
+        return nowDate.getHours()
+                    + ":" 
+                    + nowDate.getMinutes()
+                    + ":" 
+                    + nowDate.getSeconds()
+                    ;
+    }
+
+    function refreshSaveTime() {
+        $("#enregResult").html(
+            'Enregistré à ' + getNowHour() + ' <i class="icon-save"></i>'
+        );
+    }
+
+    function ajaxEnreg() {
+        topic_id = $("input[name=topic_id]").val();
+        perso_id = $("input[name=perso_id]").val();
+        tinyMCE.triggerSave();
+        content = $("#content").val();
+
+        $.ajax({
+            type: "POST",
+            url: BASE_PATH + "/forum/" + CAMPAGNE_ID + "/topic/draft",
+            data: {
+                topic_id: topic_id,
+                perso_id: perso_id,
+                content: content
+            },
+            success: function(msg){
+                refreshSaveTime();
+            },
+            error: function(msg) {}
+        });
+    }
+
+    function autoSave() {
+        var element = $('#btn-enreg');
+        if(element.length) {
+            window.setInterval(ajaxEnreg, 60000);
         }
+    }
+
+    return {
+        ajaxEnreg : ajaxEnreg,
+        autoSave: autoSave
     }
 }
 
 var draftController = draftControllerImpl();
+onLoadController.generals.push(draftController.autoSave);
