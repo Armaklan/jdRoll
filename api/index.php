a9f47082b46ef7f5782bd60b6131f040a870b300
Armaklan <lionel.zuber@armaklan.org>
2014-03-19 14:21:20 +0100

    Change autoload directory

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__.'/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 require __DIR__.'/src/jdRoll/service/dicer/dicer.php';
 require __DIR__.'/src/jdRoll/service/dicer/JetElt.php';
