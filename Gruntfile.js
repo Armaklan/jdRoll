eb694c47062cb1064df5d7a7d7bc62f5856642c5
Armaklan <lionel.zuber@armaklan.org>
2014-03-24 22:06:11 +0100

    Grunt serve php and web directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,20 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-php');
     grunt.loadNpmTasks('grunt-bower-task');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    var request = require('request');
 
     grunt.initConfig({
         php: {
             test: {
                 options: {
                     keepalive: true,
-                    open: true
+                    open: false,
+                    port: 8001,
+                    base: 'api'
                 }
             }
         },
@@ -18,6 +25,52 @@ module.exports = function(grunt) {
                 options: {
                     copy: false,
                 }
+            }
+        },
+        connect: {
+            server: {          
+              options: {
+                port: 8000,
+                base: 'web',
+                keepalive: true,
+                middleware : function(connect, options) {
+                    return [
+                        function(req,res,next) {
+                            if (req.url.substring(0,5) == '/api/'){
+                                var targetUrl = 'http://localhost:8001/'+req.url.substring(5);
+                                console.log(targetUrl);
+                                request(targetUrl, function (err, response, body) {
+                                    if (!err && response.statusCode == 200) {
+                                        res.end(body);
+                                    } else {
+                                        res.statusCode = response.statusCode;
+                                        res.end();
+                                    }
+                                });
+                            } else {
+                                return next();
+                            }
+                        },
+                        connect.static(require('path').resolve('web'))
+                    ];
+                  }
+              }
+            }
+        },
+        concurrent: {
+            serve: ['watch', 'connect', 'php'],
+            options: {
+                limit: 5,
+                logConcurrentOutput: true
+            }
+        },
+        watch: {
+            src: {
+                files: ['web/**/*.*', 'api/**/*.php'],
+                tasks: [],
+                options: {
+                    livereload: true,
+                },
             }
         },
         uglify: {
@@ -44,7 +97,7 @@ module.exports = function(grunt) {
     grunt.registerTask('prepare', ['bower']);
     grunt.registerTask('dev', ['php']);
     grunt.registerTask('dist', ['uglify']);
-    grunt.registerTask('default', ['php']);
+    grunt.registerTask('default', ['concurrent:serve']);
 
 };
 
