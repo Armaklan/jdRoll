e8cbe46f9a7de0c0a61f2a57d184c6421215107c
gailin24px <gajaf@hotmail.com>
2014-04-08 22:34:01 +0200

    Correction d'un bug sur la fermeture de la fenêtre d'authen

--- web/js/app/directives/userControl.js
+++ web/js/app/directives/userControl.js
@@ -1,8 +1,15 @@
 app.directive('jdrollAuthform', function() {
     return {
-      templateUrl: 'views/AuthForm.html'
-    };
-  });
+      templateUrl: 'views/AuthForm.html',
+	  link: function (scope, elem, attrs) {
+	  
+	    //On stoppe la propagation de l'événement de click pour éviter la fermeture intempestive de la fenêtre d'auth
+		elem.bind('click', function (e) {
+                e.stopPropagation();
+            });
+		}
+	};
+});
   
 app.directive('jdrollAlert', function() {
     return {
