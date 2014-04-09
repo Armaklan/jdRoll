b89be67d23f8f02820ea65b2144fd81409f1b6c3
Armaklan <lionel.zuber@armaklan.org>
2014-04-09 09:35:06 +0200

    Enhance META and HEAD - Title, Favicon, Description, ...

--- web/js/app/controllers/MainController.js
+++ web/js/app/controllers/MainController.js
@@ -2,6 +2,7 @@ angular.module("jdRoll.controller.main", ["jdRoll.service.session"]).
 controller('MainController',function($rootScope, $scope, SessionService, Errors) {
 
     $rootScope.campaignSpace = false;
+    $rootScope.title = 'jdRoll - Jeu de rôle par forum';
     $scope.authentInfo=SessionService.getAuthentInformation();
     $scope.errors = Errors.list;
 
