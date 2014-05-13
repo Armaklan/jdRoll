688dfb3520581e811e2527e51408a527a8f9ff34
Armaklan <lionel.zuber@armaklan.org>
2014-05-13 13:39:18 +0200

    Prepare ajax save in client

--- js/controller/feedback.js
+++ js/controller/feedback.js
@@ -3,8 +3,51 @@ var feedbackService = (function(){
 
     var component = {};
 
+    var baseUrl = BASE_PATH + "/feedback/";
+
+    var getUrlToPush = function(campagne, id) {
+        return baseUrl;
+    };
+
     var getModal = function() {
         return $("#feedbackPopup");
+    };
+
+    var getTitle = function() {
+        return $('#feedbackPopup feedbackTitle').val();
+    };
+
+    var getContent = function() {
+        return $('#feedbackPopup feedbackContent').val();
+    };
+
+    var resetModal = function() {
+        $('#feedbackPopup feedbackTitle').val('');
+        $('#feedbackPopup feedbackContent').val('');
+    };
+
+    var setModalMsg = function(msg) {
+        return $("#feedbackPopup msg").html(msg);
+    };
+
+    var saveFeedback = function() {
+        return $.ajax({
+            type: "POST",
+            url: getUrlToPush(),
+            data: {
+                title: getTitle(),
+                content: getContent()
+            }
+        });
+    };
+
+    component.pushFeedback = function() {
+        setModalMsg('');
+        saveFeedback().
+        done(function(){
+            setModalMsg('<div class="alert alert-success">Merci pour votre contribution !</div>');
+            resetModal();
+        });
     };
 
     component.openModal = function() {
