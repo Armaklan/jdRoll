c00801a0520efb8cb43c747ed28a0de214640268
Armaklan <lionel.zuber@armaklan.org>
2014-07-18 14:23:50 +0200

    CHANGE : Vagrant use standard bootstrap script

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,26 +1,41 @@
-echo "Avant d'exécuter ce script il faut : "
-echo " - Installer Mysql"
-echo " - Installer php"
-echo " - Installer node"
-echo "Voulez vous continuer (y/n) ? "
-read cont
+if [ "$1" = "-q" ]; then
+    cont=y
+else
+    echo "Avant d'exécuter ce script il faut : "
+    echo " - Installer Mysql"
+    echo " - Installer php"
+    echo " - Installer node"
+    echo "Voulez vous continuer (y/n) ? "
+    read cont
+fi
 if [ "$cont" = "y" ]; then
     echo "******** Installation des dependances Php ********"
     curl -s http://getcomposer.org/installer | php
     php composer.phar install
     php composer.phar dumpautoload -o
     echo "******** Installation des outils ********"
-    sudo npm install -g bower grunt grunt-cli
+    if [ "$1" = "-q" ]; then
+        npm install -g bower grunt grunt-cli;
+    else
+        echo "Mots de passe root nécessaire (sudo)"
+        sudo npm install -g bower grunt grunt-cli
+    fi
     npm install
     echo "******** Installation des dependances Web ********"
-    bower install
+    bower install --allow-root
     echo "******** Initialisation de la base de données ********"
-    echo "Veuillez saisir votre utilisateur mysql : "
-    read user
-    echo "Veuillez saisir le mot de passe de la base de données : "
-    read pass
-    echo "Veuillez saisir le nom de la base de données : "
-    read db
+    if [ -z "$user" ]; then
+        echo "Veuillez saisir votre utilisateur mysql : "
+        read user
+    fi
+    if [ -z "$pass" ]; then
+        echo "Veuillez saisir le mot de passe de la base de données : "
+        read pass
+    fi
+    if [ -z "$db" ]; then
+        echo "Veuillez saisir le nom de la base de données : "
+        read db
+    fi
     echo "Suppression de la base actuel"
     mysql -u $user -p$pass -e "DROP DATABASE $db" 2> /dev/null
     mysql -u $user -p$pass -e "CREATE DATABASE $db"
--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -14,27 +14,18 @@ sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $DBPASSWD"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
 
-apt-get install -y apache2 php5 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt php5-mysql php-apc php5-cli nodejs mysql-server mysql-client phpmyadmin
+apt-get install -y python-software-properties python g++ make
+add-apt-repository -y ppa:chris-lea/node.js
+add-apt-repository -y ppa:ondrej/php5-oldstable
 
-echo -e "\n--- Setting up our MySQL user and db ---\n"
-mysql -u root -p$DBPASSWD -e "CREATE DATABASE $DBNAME"
+apt-get update
+
+apt-get install -y curl apache2 php5 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt php5-mysql php-apc php5-cli nodejs mysql-server mysql-client phpmyadmin
 
 rm -rf /var/www
 ln -fs /vagrant /var/www
 
-cd /vagrant
-echo "******** Installation d'une configuration par défaut ********"
-cp config.dist.yml config.yml
-sed "s/devdev/vagrant/g" config.yml > config.yml
-echo "******** Installation des dependances Php ********"
-curl -s http://getcomposer.org/installer | php
-php composer.phar install
-php composer.phar dumpautoload -o
-echo "******** Installation des outils ********"
-sudo npm install -g bower grunt grunt-cli
-npm install
-echo "******** Installation des dependances Web ********"
-bower install
-echo "******** Initialisation de la base de données ********"
-cat ddl.sql | mysql -u root -pvagrant -D jdRoll
-echo "******** Fin de préparation de l'environnement ********"
+export user=root
+export pass=vagrant
+export db=jdRoll
+cd /vagrant && bin/bootstrap.sh -q
