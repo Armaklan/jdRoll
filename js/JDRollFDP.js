f3c09a6a9bcd21167b13f2c08eda2cb3ab6be3be
Armaklan <lionel.zuber@armaklan.org>
2014-12-06 13:41:58 +0100

    Fix(Fdp) - Fix popup open when edit in fdp

--- js/JDRollFDP.js
+++ js/JDRollFDP.js
@@ -1,85 +1,67 @@
-function initFDP(element,showEmpty) {
+function initFDP(element, showEmpty) {
 
-element.find('a[id^="JDRollUserControlLink"]').each(function(){
+    element.find('a[id^="JDRollUserControlLink"]').each(function(index, elt) {
+        var jElt = $(elt);
+        var isEmpty = 0;
+        if (jElt.hasClass("editable-empty"))
+            isEmpty = 1;
 
-		var isEmpty = 0;
-		if($(this).hasClass("editable-empty"))
-			isEmpty = 1;
-		
-		
-		if(element.has("#" + $(this).attr('id')  +  "_hidden").length > 0)
-			{
-				$(this).text($('#' + $(this).attr('id')  +  '_hidden').val());
-				$(this).removeClass("editable-empty");
-				isEmpty = 0;
-			}
-		
-		$(this).editable({
-			value:  $('#' + $(this).attr('id')  +  '_hidden').val(),
-			success: function(response, newValue) {
-				if(newValue.city)
-				{
-					if($(this).css('color') == 'rgb(221, 17, 68)')
-								$(this).css('color','#0088cc');
-					$(this).text(newValue.city);
-					if($(this).parent().parent().has('#' + $(this).attr('id')  +  '_hidden').length > 0)
-						$(this).parent().parent().find('#' + $(this).attr('id')  +  '_hidden').val(newValue.city);
-					else
-						$(this).parent().append('<input type="hidden" id="' + $(this).attr('id')  +  '_hidden" value="' + newValue.city + '"/>');
-				}
-				else
-				{
-						
-					if(newValue.city == "")
-					{
-						if($(this).css('color') == 'rgb(0, 136, 204)')
-								$(this).css('color','#DD1144');
-						if($(this).parent().parent().has('#' + $(this).attr('id')  +  '_hidden').length > 0)
-						{
 
-							$(this).parent().parent().find('#' + $(this).attr('id')  +  '_hidden').val('');
-						}
-						else
-						{
-							$(this).parent().append("<input type='hidden' id='" + $(this).attr('id')  +  "_hidden' value='' />");
-						}
-					}
-					else
-					{
-						if(newValue == "")
-						{
-							if($(this).css('color') == 'rgb(0, 136, 204)')
-								$(this).css('color','#DD1144');
-						}
-						else
-						{
-							if($(this).css('color') == 'rgb(221, 17, 68)')
-								$(this).css('color','#0088cc');
-								
-						}
-							
-						if($(this).parent().parent().has('#' + $(this).attr('id')  +  '_hidden').length > 0)
-						{
-							
-							$(this).parent().parent().find('#' + $(this).attr('id')  +  '_hidden').val(newValue);
-						}
-						else
-						{
-							$(this).parent().append('<input type="hidden" id="' + $(this).attr('id')  +  '_hidden" value="' + newValue + '"/>');
-						}
-					}
-				}
-			}
-		});
-		
-		if(isEmpty)
-		{
-			$(this).addClass("editable-empty");
-			if(!showEmpty)
-			{
-				$(this).css("display","none");
-			}
-			
-		}
-	});
+        if (element.has("#" + jElt.attr('id') + "_hidden").length > 0) {
+            jElt.text($('#' + jElt.attr('id') + '_hidden').val());
+            jElt.removeClass("editable-empty");
+            isEmpty = 0;
+        }
+
+        jElt.editable({
+            value: $('#' + jElt.attr('id') + '_hidden').val(),
+            success: function(response, newValue) {
+                if (newValue.city) {
+                    if (jElt.css('color') == 'rgb(221, 17, 68)')
+                        jElt.css('color', '#0088cc');
+                    jElt.text(newValue.city);
+                    if (jElt.parent().parent().has('#' + jElt.attr('id') + '_hidden').length > 0)
+                        jElt.parent().parent().find('#' + jElt.attr('id') + '_hidden').val(newValue.city);
+                    else
+                        jElt.parent().append('<input type="hidden" id="' + jElt.attr('id') + '_hidden" value="' + newValue.city + '"/>');
+                } else {
+
+                    if (newValue.city == "") {
+                        if (jElt.css('color') == 'rgb(0, 136, 204)')
+                            jElt.css('color', '#DD1144');
+                        if (jElt.parent().parent().has('#' + jElt.attr('id') + '_hidden').length > 0) {
+
+                            jElt.parent().parent().find('#' + jElt.attr('id') + '_hidden').val('');
+                        } else {
+                            jElt.parent().append("<input type='hidden' id='" + jElt.attr('id') + "_hidden' value='' />");
+                        }
+                    } else {
+                        if (newValue == "") {
+                            if (jElt.css('color') == 'rgb(0, 136, 204)')
+                                jElt.css('color', '#DD1144');
+                        } else {
+                            if (jElt.css('color') == 'rgb(221, 17, 68)')
+                                jElt.css('color', '#0088cc');
+
+                        }
+
+                        if (jElt.parent().parent().has('#' + jElt.attr('id') + '_hidden').length > 0) {
+
+                            jElt.parent().parent().find('#' + jElt.attr('id') + '_hidden').val(newValue);
+                        } else {
+                            jElt.parent().append('<input type="hidden" id="' + jElt.attr('id') + '_hidden" value="' + newValue + '"/>');
+                        }
+                    }
+                }
+            }
+        });
+
+        if (isEmpty) {
+            jElt.addClass("editable-empty");
+            if (!showEmpty) {
+                jElt.css("display", "none");
+            }
+
+        }
+    });
 }
