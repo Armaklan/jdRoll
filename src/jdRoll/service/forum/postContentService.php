737585c1e823c0ecb689b316e51dae431cb21d74
Armaklan <lionel.zuber@armaklan.org>
2015-07-20 21:43:25 +0200

    Fix(Balise Private) - Gestion des accents

--- src/jdRoll/service/forum/postContentService.php
+++ src/jdRoll/service/forum/postContentService.php
@@ -113,7 +113,7 @@ class PostContentService {
                                              }
                                              else
                                              {
-                                               $users = preg_split("#,#", $matches[2]);
+                                               $users = preg_split("#,#", html_entity_decode($matches[2]));
                                                foreach($users as $user)
                                                {
                                                  if(strcasecmp($login,trim($user)) == 0 || strcasecmp($perso['name'],trim($user)) == 0)
