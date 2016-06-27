0615bb830495cc9a9eaec3dc63e9681a059a49a5
Armaklan <lionel.zuber@armaklan.org>
2016-06-27 21:35:47 +0200

    Fix(Carte) - Bug création de la carte

--- js/angular/carte/ctrlCarteCreator.js
+++ js/angular/carte/ctrlCarteCreator.js
@@ -6,23 +6,25 @@ function ($scope, $http, $location) {
     $scope.carte = {
         campagne_id: $scope.getCampagneId(),
         published: '1',
-        markers: []
-    }
+        config: {
+            markers: []
+        }
+    };
 
     $scope.messages = [];
 
     $scope.create = function(){
         $scope.messages = [];
         if( ! $scope.carte.name){
-            $scope.messages.push('Le nom est obligatoire.')
+            $scope.messages.push('Le nom est obligatoire.');
         }
         if( ! $scope.carte.image){
-            $scope.messages.push('L\'image est obligatoire.')
+            $scope.messages.push('L\'image est obligatoire.');
         }
         if( ! $scope.messages.length){
             $http.post('carte/save', $scope.carte).success(function(carte){
-                $location.path('/carte/' + carte.id)
+                $location.path('/carte/' + carte.id);
             });
         }
-    }
+    };
 }]);
