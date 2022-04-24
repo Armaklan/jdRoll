e97b2ca7072d19c7f2570f251b81e62504b376dd
Armaklan <lionel.zuber@armaklan.org>
2022-04-24 16:11:34 +0200

    tech(web) - mise en place storybook et webcomponent

---
+++ web/stories/Button.stories.js
@@ -0,0 +1,16 @@
+import { html } from 'lit';
+import '../components/Button';
+
+export default {
+  title: 'jdRoll/Button',
+  argTypes: {},
+};
+
+// More on component templates: https://storybook.js.org/docs/web-components/writing-stories/introduction#using-args
+const Template = (args) => html`<jd-button label=${args.label}></jd-button>`;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/web-components/writing-stories/args
+Primary.args = {
+  label: 'Button',
+};
